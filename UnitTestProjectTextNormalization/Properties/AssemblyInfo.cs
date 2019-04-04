79236d4ec0dcad836cb9125c0e7c4d80b074c44f
MidRider <49253376+MidRider@users.noreply.github.com>
2019-04-04 21:04:14 +0300

    Добавление проекта юнит-тестирования

---
+++ UnitTestProjectTextNormalization/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectTextNormalization")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectTextNormalization")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c95437c-09b1-4e38-9368-e0f763a6b220")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectTextNormalization/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextNormalization;
+
+namespace UnitTestProjectTextNormalization
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodAppRegExp()
+        {
+            // исходные данные
+            string expected = "HeLL o wOrl d";
+
+            // получение значения с помощью тестируемого метода
+            string actual = FormNormalization.AppRegExp("HeLL()o wOrl*/d");
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
